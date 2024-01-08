190411db003a2dc6dcda31f6a829cdf3fea26f90
Jacob Perkins <jacob.perkins94@gmail.com>
2024-01-08 13:12:11 -0500

    store results of query in memory

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ XElement notesFromFile = XElement.Load(@"data1.xml");
 
 IEnumerable<XElement> notes = notesFromFile.Elements();
 
-
 // Get all notes
 
 foreach ( var note in notes )
@@ -29,3 +28,17 @@ foreach (XElement el in archivedNotes)
 {
   Console.WriteLine(el);
 }
+
+
+
+/*******************************/
+
+IEnumerable<XElement> notesFromJacob = from e in notesFromFile.Elements("note")
+                                  where (string)e?.Element("from")?.Value.ToLower() == "jacob".ToLower()
+                                  select e;
+
+var messagesFromJacob = notesFromJacob?.ToList<XElement>();
+
+Console.WriteLine(messagesFromJacob[0]);
+
+Console.WriteLine(messagesFromJacob?.First().Element("heading").Value);
