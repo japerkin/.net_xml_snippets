a452cbc94008654af30537335c16b2f06f253a55
Jacob Perkins <jacob.perkins94@gmail.com>
2024-01-08 12:53:35 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Xml;
+using System.Xml.Linq;
+
+XElement notesFromFile = XElement.Load(@"data1.xml");
+
+IEnumerable<XElement> notes = notesFromFile.Elements();
+
+
+// Get all notes
+
+foreach ( var note in notes )
+{
+  Console.WriteLine( note?.Element("heading")?.Value );
+}
+
+
+// Only get the archived notes.
+
+IEnumerable<XElement> archivedNotes = from el in notesFromFile.Elements("note")
+                                      where (string)el?.Attribute("archived") == "true"
+                                      select el;
+                    
+
+Console.WriteLine(archivedNotes.Count());
+
+foreach (XElement el in archivedNotes)
+{
+  Console.WriteLine(el);
+}
